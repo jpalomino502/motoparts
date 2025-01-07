9ace26aaca8e2ba9def3fd58d83dc10eea6e1a07
jpalomino502 <joseph.palomino0102@gmail.com>
2025-01-06 22:12:45 -0500

    v1.0

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,5 +1,5 @@
 import { useContext } from 'react';
-import { AuthContext } from '../context/AuthContext'; // Asegúrate de que la ruta sea correcta
+import { AuthContext } from '../context/AuthContext';
 
 export const useAuth = () => {
   const context = useContext(AuthContext);
